08cd954d30916fea6ebb475cdc51553d3f97ee9a
Nick Payne <nick.payne89@gmail.com>
2019-02-01 16:44:11 +0800

    fix test to check travis

--- client/src/app/surveyconfig/configGenerator.test.js
+++ client/src/app/surveyconfig/configGenerator.test.js
@@ -62,7 +62,7 @@ test('generates one page with a multiple text field of category "contact"', () =
         }]
     };
 
-    expect(pageConfig).not.toEqual(expectedPageConfig);
+    expect(pageConfig).toEqual(expectedPageConfig);
 });
 
 
